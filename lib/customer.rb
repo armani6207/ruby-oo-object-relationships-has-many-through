13b664852013276432106919b03928c36f284550
Armando <rojas.armani@gmail.com>
2020-12-03 07:11:19 -0500

    commit tree 6ac8829d8177b1f3c3ecba38c340f522145955e1

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
 class Customer
+    attr_accessor :name, :age
   
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.find_all {|x| x.customer == self}
+    end
+
+    def waiters
+        meals.collect {|x| x.waiter}.uniq
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Waiter
+    attr_accessor :name, :age
+  
+    @@all = []
 
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.find_all {|x| x.waiter == self}
+    end
+
+    def best_tipper
+        tips = []
+        meals.each {|x| tips << x.tip}
+        meals[tips.index(tips.max)].customer
+    end
 end
